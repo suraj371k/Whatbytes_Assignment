4ee47a4c08a5ce93a4a5771a7bc3c3383e80d5d8
suraj371k <surajkushwaha371@gmail.com>
2025-06-01 00:21:27 +0530

    Adding product data

--- src/utils/productData.ts
+++ src/utils/productData.ts
@@ -1,68 +1,102 @@
+import camera from "./camera.avif";
+import c_shoes from "./casual_shoes.avif";
+import chair from "./chair.avif";
+import earbud from "earpods.avif";
+import formal_shoes from "./formal_shoes.avif";
+import headphone from "./headphone.avif";
+import lamp from "./lamp.avif";
+import running_shoes from "./running_shoes.avif";
+import shirt from "./running_shoes.avif";
+import sofa from "./sofa.avif";
+import top from "./top.avif";
+import tshirt from "./tshirt.avif";
+
 export const items = [
   {
     id: 1,
     name: "Running Shoes",
+    image: running_shoes,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "clothing",
   },
   {
     id: 2,
-    name: "Running Shoes",
+    name: "Formal Shoes",
+    image: formal_shoes,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "clothing",
   },
   {
     id: 3,
-    name: "Running Shoes",
+    name: "Casual Shoes",
+    image: c_shoes,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "clothing",
   },
   {
     id: 4,
-    name: "Running Shoes",
+    name: "Camera",
+    image: camera,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "electronics",
   },
   {
     id: 5,
-    name: "Running Shoes",
+    name: "Earbud",
+    image: earbud,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "electronics",
   },
   {
     id: 6,
-    name: "Running Shoes",
+    name: "Headphone",
+    image: headphone,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "electronics",
+  },
+  {
+    id: 7,
+    name: "Sofa",
+    image: sofa,
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
+    price: 99,
+    category: "home",
   },
   {
     id: 8,
-    name: "Running Shoes",
+    name: "Chair",
+    image: chair,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "home",
   },
   {
     id: 9,
-    name: "Running Shoes",
+    name: "Study Lamp",
+    image: lamp,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
   },
   {
     id: 10,
-    name: "Running Shoes",
+    name: "Shirt",
+    image: shirt,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
+    category: "clothing",
   },
-    {
+  {
     id: 11,
-    name: "Running Shoes",
+    name: "T-Shirt",
+    image: tshirt,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
     price: 99,
-  },
-    {
-    id: 12,
-    name: "Running Shoes",
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam unde deserunt voluptatum.`,
-    price: 99,
+    category: "clothing",
   },
 ];
